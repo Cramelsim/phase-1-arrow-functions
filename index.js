5404327dcd3f006d1013ddc886bfdfb5140c938f
MELODY SIMWA <simwamelody@gmail.com>
2024-11-28 17:54:15 +0300

    create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = (a,b) => {
+    if (b === 0) throw new Error("Cannot divide by zero");
+    return a/b;
+};
+
+const square=(x)=> {
+    if (x===4);
+    return x*x;
+}
+
+const add=( a, b)=>(a+b) 
